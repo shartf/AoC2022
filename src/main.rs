09f60bd12c2b6b235af0222f5bf3fc7141f6b09e
shartf <shartf@users.noreply.github.com>
2022-11-17 22:57:09 +0100

    need to write files still

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use utils::download_file;
+use utils::{check_for_file, download_file};
 pub mod utils;
 
 fn main() {
-    download_file().unwrap();
+    // download_file().unwrap();
+    check_for_file("2");
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,19 +1,45 @@
-use std::{env, fs, io};
+use std::{
+    env,
+    fs::{self, File},
+    io,
+    path::Path,
+};
 
 pub fn read_api_key() -> io::Result<String> {
     let api_path = "secret/api.txt";
     let mut curr_path = env::current_dir()?;
     curr_path.push(api_path);
+    // println!("{}", curr_path.display());
     let contents =
         fs::read_to_string(curr_path).expect("... not able to read api key. Is it there?");
     Ok(contents)
 }
 
-pub fn download_file() -> Result<(), ureq::Error> {
-    let body: String = ureq::get("https://adventofcode.com/2016/day/2/input")
+pub fn download_file(day_id: &str) -> Result<String, ureq::Error> {
+    let prefix: &str = "https://adventofcode.com/2016/day/";
+    let postfix: &str = "/input";
+    let binding = format!("{prefix}{day_id}{postfix}",).to_string();
+    let body: String = ureq::get(&binding)
         .set("Cookie", &read_api_key().unwrap())
         .call()?
         .into_string()?;
-    println!("Headers: \n{:#?}", body);
-    Ok(())
+    // println!("Headers: \n{:#?}", body);
+    Ok(body)
+}
+
+pub fn check_for_file(day_id: &str) {
+    let binding = format!("{}{}{}", "files/day", day_id, ".txt").to_string();
+    let path = Path::new(&binding);
+    //exists is prone to TOCTOU and should not be used in serious code
+    match path.exists() {
+        true => println!("file is here"),
+        false => {
+            if let Ok(body) = download_file(day_id) {
+                println!("body is: {body}")
+                // TODO write a file and write that download is finished
+            } else {
+                panic!()
+            }
+        }
+    }
 }
