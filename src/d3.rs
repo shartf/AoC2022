2ef1e17634de3c17fb98eb9e9e0cbbe9a6aa80a6
shartf <shartf@users.noreply.github.com>
2022-12-05 00:12:58 +0100

    d3-2 done. Ugly, ugly!

--- src/d3.rs
+++ src/d3.rs
@@ -1,23 +1,23 @@
-use std::str::FromStr;
-
 use itertools::Itertools;
 
 pub fn day_3() -> anyhow::Result<()> {
     let input: String = std::fs::read_to_string("files/day3.txt").unwrap();
     let parsed: Vec<&str> = input.split('\n').collect_vec();
     let mut acc: u32 = 0;
-    for v in parsed {
-        let len = v.len();
-        let fst = &v[..len / 2];
-        let scd = &v[len / 2..];
+    let mut idx = 0;
+    while idx < parsed.len() - 3 {
         // println!("v.len is {}, start{} === {} stop", len, fst, scd);
+        let fst = parsed[idx];
+        let scd = parsed[idx + 1];
+        let trd = parsed[idx + 2];
         'charloop: for c in fst.chars() {
-            if scd.contains(c) {
+            if scd.contains(c) && trd.contains(c) {
                 acc += mapping(c);
                 // need to break on first hit, otherwise we get duplicates
                 break 'charloop;
             }
         }
+        idx += 3;
     }
     println!("acc is {acc}");
 
