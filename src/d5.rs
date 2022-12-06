77b98e12a5394b991ed93a5252d5332fe9afd9d1
Sergej Hartfil <sergej.hartfil@sap.com>
2022-12-06 17:59:04 +0100

    Nikolaus brought this

---
+++ src/d5.rs
@@ -0,0 +1,73 @@
+pub fn day_5() -> anyhow::Result<()> {
+    let mut one: Vec<String> = vec![
+        'N'.to_string(),
+        'R'.to_string(),
+        'G'.to_string(),
+        'P'.to_string(),
+    ];
+    let mut two: Vec<String> = vec![
+        'J'.to_string(),
+        'T'.to_string(),
+        'B'.to_string(),
+        'L'.to_string(),
+        'F'.to_string(),
+        'G'.to_string(),
+        'D'.to_string(),
+        'C'.to_string(),
+    ];
+    let mut three: Vec<String> = vec!['M'.to_string(), 'S'.to_string(), 'V'.to_string()];
+    let mut four: Vec<String> = vec![
+        'L'.to_string(),
+        'S'.to_string(),
+        'R'.to_string(),
+        'C'.to_string(),
+        'Z'.to_string(),
+        'P'.to_string(),
+    ];
+    let mut five: Vec<String> = vec![
+        'P'.to_string(),
+        'S'.to_string(),
+        'L'.to_string(),
+        'V'.to_string(),
+        'C'.to_string(),
+        'W'.to_string(),
+        'D'.to_string(),
+        'Q'.to_string(),
+    ];
+    let mut six: Vec<String> = vec![
+        'C'.to_string(),
+        'T'.to_string(),
+        'N'.to_string(),
+        'W'.to_string(),
+        'D'.to_string(),
+        'M'.to_string(),
+        'S'.to_string(),
+    ];
+    let mut seven: Vec<String> = vec![
+        'H'.to_string(),
+        'D'.to_string(),
+        'G'.to_string(),
+        'W'.to_string(),
+        'P'.to_string(),
+    ];
+    let mut eight: Vec<String> = vec![
+        'Z'.to_string(),
+        'L'.to_string(),
+        'P'.to_string(),
+        'H'.to_string(),
+        'S'.to_string(),
+        'C'.to_string(),
+        'M'.to_string(),
+        'V'.to_string(),
+    ];
+    let mut nine: Vec<String> = vec![
+        'R'.to_string(),
+        'P'.to_string(),
+        'F'.to_string(),
+        'L'.to_string(),
+        'W'.to_string(),
+        'G'.to_string(),
+        'Z'.to_string(),
+    ];
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![allow(non_snake_case)]
 use utils::check_for_file;
-pub mod d4;
+pub mod d5;
 pub mod utils;
 
 fn main() {
-    check_for_file("4");
-    d4::day_4().unwrap();
+    check_for_file("5");
+    d5::day_5().unwrap();
 }
