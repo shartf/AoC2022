7e42d51fe0b0c45a58a0ff8b4f6f259855cea5b2
shartf <shartf@users.noreply.github.com>
2022-12-08 23:11:35 +0100

    WIP

--- src/d6.rs
+++ src/d6.rs
@@ -3,7 +3,7 @@ use itertools::Itertools;
 pub fn day_6() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("files/day6.txt").unwrap();
 
-    let res = part_one(input);
+    let res = part_two(input);
     println!("{res}");
     Ok(())
 }
@@ -16,6 +16,22 @@ fn part_one(parsed: String) -> usize {
             fst != scd && fst != trd && fst != fth && scd != trd && scd != fth && trd != fth
         });
     idx.unwrap() + 4
+}
+
+fn part_two(parsed: String) -> usize {
+    let bottom: usize = 0;
+    let upper: usize = 16;
+    while upper <= parsed.chars().count() {
+        let slice = &parsed[bottom..=upper];
+        let len = slice.len();
+        if len > slice.unique().len() {
+            break;
+        } else {
+            bottom += 1;
+            upper += 1;
+        }
+    }
+    upper
 }
 
 #[cfg(test)]
@@ -31,5 +47,18 @@ mod tests {
         assert_eq!(part_one(input_2), 6 as usize);
         assert_eq!(part_one(input_3), 10 as usize);
         assert_eq!(part_one(input_4), 11 as usize);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input_1 = String::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+        let input_2 = String::from("bvwbjplbgvbhsrlpgdmjqwftvncz");
+        let input_3 = String::from("nppdvjthqldpwncqszvftbrmjlhg");
+        let input_4 = String::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+
+        assert_eq!(part_two(input_1), 19 as usize);
+        assert_eq!(part_two(input_2), 23 as usize);
+        assert_eq!(part_two(input_3), 23 as usize);
+        assert_eq!(part_two(input_4), 29 as usize);
     }
 }
